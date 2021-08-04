cc8baa72559fa480b4d18ef7eded34f049eb4bf3
吕海涛 <ihaitao@qq.com>
2021-08-04 08:14:17 +0800

    支持生成 js doc

--- main.go
+++ main.go
@@ -1,14 +1,11 @@
 package main
 
 import (
-	"io"
-	"io/ioutil"
+	"flag"
 	"log"
 	"os"
-	"strings"
 
-	"github.com/golang/protobuf/proto"
-	plugin "github.com/golang/protobuf/protoc-gen-go/plugin"
+	"google.golang.org/protobuf/compiler/protogen"
 )
 
 func init() {
@@ -16,59 +13,16 @@ func init() {
 }
 
 func main() {
-	req := readGenRequest(os.Stdin)
+	g := newGenerator()
 
-	params, err := parseCommandLineParams(req.GetParameter())
-	if err != nil {
-		Error(err, "could not parse parameters")
-	}
+	var flags flag.FlagSet
 
-	g := newGenerator(params)
+	// flags.StringVar(&g.OptionPrefix, "option_prefix", "sniper", "")
+	// flags.StringVar(&g.RootPackage, "root_package", "sniper", "")
+	// flags.BoolVar(&g.ValidateEnable, "validate_enable", false, "")
 
-	resp := g.Generate(req)
+	protogen.Options{
+		ParamFunc: flags.Set,
+	}.Run(g.Generate)
 
-	writeResponse(os.Stdout, resp)
-}
-
-func readGenRequest(r io.Reader) *plugin.CodeGeneratorRequest {
-	data, err := ioutil.ReadAll(r)
-	if err != nil {
-		Error(err, "reading input")
-	}
-
-	req := new(plugin.CodeGeneratorRequest)
-	if err = proto.Unmarshal(data, req); err != nil {
-		Error(err, "parsing input proto")
-	}
-
-	if len(req.FileToGenerate) == 0 {
-		Fail("no files to generate")
-	}
-
-	return req
-}
-
-func writeResponse(w io.Writer, resp *plugin.CodeGeneratorResponse) {
-	data, err := proto.Marshal(resp)
-	if err != nil {
-		Error(err, "marshaling response")
-	}
-	_, err = w.Write(data)
-	if err != nil {
-		Error(err, "writing response")
-	}
-}
-
-// Error print error and exit
-func Error(err error, msgs ...string) {
-	s := strings.Join(msgs, " ") + ":" + err.Error()
-	log.Print("error:", s)
-	os.Exit(1)
-}
-
-// Fail print error and exit
-func Fail(msgs ...string) {
-	s := strings.Join(msgs, " ")
-	log.Print("error:", s)
-	os.Exit(1)
 }
